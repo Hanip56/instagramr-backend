e7c6940895437dde60fe2acc5513e2c9a92296e0
hanip303 <hanipalhapidz56@gmail.com>
2023-07-07 09:53:01 +0700

    add socket.io to communicate with frontend without database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 require("colors");
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
 const { corsOptions } = require("./config/corsOptions");
 const { credentials } = require("./middleware/credentials");
@@ -11,7 +12,39 @@ const { connectDB } = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+const server = http.createServer(app);
 connectDB();
+
+// socket io
+const io = require("socket.io")(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on("send-message", ({ roomId, recipients, sender, text }, cb) => {
+    const currentDate = Date.now();
+    recipients.forEach((recipient) => {
+      // const newRecipients = recipients.filter((r) => r !== recipient);
+      // newRecipients.push(id);
+      socket.broadcast.to(recipient._id).emit("receive-message", {
+        roomId,
+        recipients,
+        sender,
+        text,
+        createdAt: currentDate,
+      });
+    });
+
+    // Assuming recipient received the message successfully
+    if (cb) {
+      cb({ success: true, createdAt: currentDate });
+    }
+  });
+});
 
 // if the server allows the request to include credentials add this
 app.use(credentials);
@@ -29,6 +62,6 @@ app.use("/api/post", require("./routes/postRoutes"));
 
 app.use(errorHandler);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`.yellow.underline);
 });
